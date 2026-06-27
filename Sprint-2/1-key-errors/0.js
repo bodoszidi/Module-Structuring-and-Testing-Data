9fede25bf7159d958621dcf6fa1cc398667b85fe
bodoszidi <szidibee@gmail.com>
2026-06-27 18:35:56 +0100

    explained the function

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
+//the function is there for capitalise the given string
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
@@ -9,5 +10,11 @@ function capitalise(str) {
   return str;
 }
 
+console.log(capitalise("hello"))
 // =============> write your explanation here
+  // Identifier 'str' has already been declared - means the variable already exist - with the let you don't need to declare this again
 // =============> write your new code here
+  //function capitalise(str) {
+  // str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  //return str;
+  //}
