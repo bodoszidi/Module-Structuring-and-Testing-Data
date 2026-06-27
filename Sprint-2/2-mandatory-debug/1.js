a72a8d2378c46610426fafa5b4708a6c5cf7532d
bodoszidi <szidibee@gmail.com>
2026-06-27 18:36:56 +0100

    explained the function

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,18 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// the function will sum the given parameters 
 
 function sum(a, b) {
-  return;
-  a + b;
+  return  a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+  // unable to return nothing - the statement is not given - has to be in the same line with the return
 // Finally, correct the code to fix the problem
+
 //  =============> write your new code here
+  // function sum(a, b) {
+  //return a + b;
+  //} 
