8e51dec6947142f1fc9ed9dde0079cfb97c324ae
bodoszidi <szidibee@gmail.com>
2026-06-27 18:36:43 +0100

    explaining the function

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,4 +1,6 @@
 // Predict and explain first...
+//function is there to multiply the given arguments in the console log line 10
+
 
 // =============> write your prediction here
 
@@ -9,6 +11,11 @@ function multiply(a, b) {
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
-
+  // we would have an error message as we would need to return the statement
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+  //function multiply(a, b) {
+  //return(a * b);
+  //}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
