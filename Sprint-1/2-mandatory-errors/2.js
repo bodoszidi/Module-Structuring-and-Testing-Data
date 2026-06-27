ef80dd1ed30cbc7036e24475db875d74e8c2fea0
bodoszidi <szidibee@gmail.com>
2026-06-27 17:36:19 +0100

    fixed the bug on the code - the variable wasn't declared before logging the string

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,8 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+// the variable cityOfBirth was after the console log which means you cannot log before you actually have the variable
