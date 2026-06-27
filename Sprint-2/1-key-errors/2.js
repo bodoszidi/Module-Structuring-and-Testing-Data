d0520a0f7946b1f9b9845adba87e908a8bae5c75
bodoszidi <szidibee@gmail.com>
2026-06-27 18:36:30 +0100

    explanation of the function

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,5 +1,6 @@
 
 // Predict and explain first BEFORE you run any code...
+    //function is there to calculate the square parameter
 
 // this function should square any number but instead we're going to get an error
 
@@ -10,11 +11,18 @@ function square(3) {
 }
 
 // =============> write the error message here
+    // we are returning the num - but the parameter already given as an argument not a parameter 3 - 
+    // for the function to recognize num we would need to give this to the function as a parameter
 
 // =============> explain this error message here
+    //unexpected number - the function has an unexpected argument
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+    //function square(num) {
+    //return num * num;
+    //}
+    //square(3)
 
 
