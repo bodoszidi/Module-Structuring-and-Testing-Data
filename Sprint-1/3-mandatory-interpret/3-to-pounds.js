7dd742ec1d982bf508a6c8ea6c559bfa5ab5c6cb
bodoszidi <szidibee@gmail.com>
2026-06-27 18:11:14 +0100

    explained the code line by line

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,22 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+//
+// 3.const penceStringWithoutTrailingP = penceString.substring(
+//  0,
+//  penceString.length - 1
+// ); assigned the string to a new variable with out the P - by using the substring method - the .length will count the number of the characters and only display 1 before last
+//
+// 8. const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0"); 
+// using the pedStart method we ensure to fill the gas - we can add 0 on this occasion to our string beginning the the number of length does not match with 3 characters - fill in the gaps
+// 9. const pounds = paddedPenceNumberString.substring(
+//  0,
+// paddedPenceNumberString.length - 2
+//); again we create a new variable to only store the first character of the string 
+//  
+// 14. const pence = paddedPenceNumberString
+//  .substring(paddedPenceNumberString.length - 2)
+//  .padEnd(2, "0");
+// creating a new variable to assign only the last two character of the string - we also use the padEnd method to fill the end gaps with zeros
+//
+// 18. we call the newly created variables with consol.log to display this as a new string on the console
