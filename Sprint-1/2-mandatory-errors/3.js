2ca0d04500bb3ebd38f716a82604b5b17303362c
bodoszidi <szidibee@gmail.com>
2026-06-27 17:37:43 +0100

    using slice method to have display only the required amount

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,9 +1,14 @@
-const cardNumber = 4533787178994213;
+const cardNumber = "4533787178994213";
 const last4Digits = cardNumber.slice(-4);
+console.log(last4Digits);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
 // Before running the code, make and explain a prediction about why the code won't work
 // Then run the code and see what error it gives.
+//const last4Digits = cardNumber.slice(-4);
+                               
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
+// The error comes up cause the value of the cardNumber is not a string it's a number
+//Once you change the value to a string the .slice()
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
