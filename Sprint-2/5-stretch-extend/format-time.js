b9700847abba1ca6655c8d3a3daa8cc48466e4fe
bodoszidi <szidibee@gmail.com>
2026-07-01 21:36:45 +0100

    wrote more test cases - found the bug and updated the code

--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -1,25 +1,58 @@
 // This is the latest solution to the problem from the prep.
 // Make sure to do the prep before you do the coursework
 // Your task is to write tests for as many different groups of input data or edge cases as you can, and fix any bugs you find.
+function pad(num) {
+  let numString = num.toString();
+  while (numString.length < 2) {
+    numString = "0" + numString;
+  }
+  return numString;
+}
 
 function formatAs12HourClock(time) {
   const hours = Number(time.slice(0, 2));
-  if (hours > 12) {
-    return `${hours - 12}:00 pm`;
+  const minutes = time.slice(3, 5);
+  if (hours === 0) {
+    return `12:${minutes} am`;
+  } else if (hours > 12) {
+    return `${pad(hours - 12)}:${minutes} pm`;
+  } else if (hours === 12) {
+    return `12:${minutes} pm`;
   }
-  return `${time} am`;
+  return `${pad(hours)}:${minutes} am`;
 }
 
-const currentOutput = formatAs12HourClock("08:00");
-const targetOutput = "08:00 am";
+const currentOutput = formatAs12HourClock("06:00");
+const targetOutput = "06:00 am";
 console.assert(
   currentOutput === targetOutput,
   `current output: ${currentOutput}, target output: ${targetOutput}`
 );
 
-const currentOutput2 = formatAs12HourClock("23:00");
-const targetOutput2 = "11:00 pm";
+const currentOutput2 = formatAs12HourClock("12:00");
+const targetOutput2 = "12:00 pm";
 console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
+);
+
+const currentOutput3 = formatAs12HourClock("00:00");
+const targetOutput3 = "12:00 am";
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
+);
+
+const currentOutput4 = formatAs12HourClock("22:59");
+const targetOutput4 = "10:59 pm";
+console.assert(
+  currentOutput4 === targetOutput4,
+  `current output: ${currentOutput4}, target output: ${targetOutput4}`
+);
+
+const currentOutput5 = formatAs12HourClock("21:30");
+const targetOutput5 = "09:30 pm";
+console.assert(
+  currentOutput5 === targetOutput5,
+  `current output: ${currentOutput5}, target output: ${targetOutput5}`
 );
